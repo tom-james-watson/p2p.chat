f1286a498e6a74d7cffcf5d9112cbe1c33a5d33c
Tom Watson <tom@tomjwatson.com>
2018-04-27 02:47:35 +0200

    improve awaiting peers copy

--- src/js/components/awaiting-peers.js
+++ src/js/components/awaiting-peers.js
@@ -7,8 +7,8 @@ export default (props) => {
 
   return (
     <div id='awaiting-peers' className='hero container'>
-      <h3>Searching for peers...</h3>
-      <p>Invite to start a video call</p>
+      <h3>Waiting for peers...</h3>
+      <p>Share the link to start a video call</p>
       <CopyLink />
     </div>
   )
