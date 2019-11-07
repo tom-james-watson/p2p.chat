6f29b3ba67f9a1f228081f0b8aa29bc30f9fdce7
Tom Watson <tom@tomjwatson.com>
2019-11-07 13:49:43 +0100

    fix getUserMedia deprecation

--- src/js/lib/media.js
+++ src/js/lib/media.js
@@ -1,26 +1,15 @@
-import getMedia from 'getusermedia'
-
 async function getMediaStream(opts) {
-  return new Promise(async (resolve, reject) => {
-    getMedia(opts, function (err, stream) {
-      if (err) {
-        return reject(err)
-      }
-
-      resolve(stream)
-    })
-  })
+  return navigator.mediaDevices.getUserMedia(opts)
 }
 
 export default async function getMyStream() {
 
   const video = {
-    mandatory: {
-      width: {min: 640, ideal: 1280, max: 1920},
-      height: {min: 360, ideal: 720, max: 1080},
-      maxAspectRatio: 4/3,
-      frameRate: {ideal: 15, max: 24},
-    }
+    facingMode: "user",
+    width: {min: 640, ideal: 1280, max: 1920},
+    height: {min: 360, ideal: 720, max: 1080},
+    maxAspectRatio: 4/3,
+    frameRate: {ideal: 15, max: 24},
   }
   const audio = {
     autoGainControl: true,
@@ -32,15 +21,20 @@ export default async function getMyStream() {
 
   try {
     // Try and get video and audio
+    console.log('try video and audio')
     const stream = await getMediaStream({video, audio})
     return {myStream: stream, audioEnabled: true, videoEnabled: true}
-  } catch (e) {
+  } catch (err) {
+    console.error(err)
     try {
+      console.log('try just audio')
       // If that fails, try just audio
       const stream = await getMediaStream({audio})
       return {myStream: stream, audioEnabled: true, videoEnabled: false}
     } catch (err) {
+      console.error(err)
       try {
+        console.log('try just video')
         // If that fails, try just video
         const stream = await getMediaStream({video})
         return {myStream: stream, audioEnabled: false, videoEnabled: true}
