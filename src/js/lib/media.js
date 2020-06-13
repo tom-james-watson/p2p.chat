aec2d4b9b36f86bfe58336015f5f33cf7c9f6774
Tom Watson <tom@tomjwatson.com>
2020-06-13 19:41:12 +0200

    process audio less
    This is in an atempt to both get better audio quality and to fix a bug
    where both chrome and firefox on mobile have unusably distorted audio.

--- src/js/lib/media.js
+++ src/js/lib/media.js
@@ -8,13 +8,16 @@ export default async function getMyStream() {
     facingMode: "user",
     width: {min: 640, ideal: 1280, max: 1920},
     height: {min: 360, ideal: 720, max: 1080},
+    maxAspectRatio: 4/3,
     frameRate: {ideal: 15, max: 24},
   }
   const audio = {
-    autoGainControl: true,
-    sampleRate: {ideal: 48000, min: 35000},
-    echoCancellation: true,
+    autoGainControl: false,
     channelCount: {ideal: 1},
+    echoCancellation: false,
+    googAutoGainControl: false,
+    noiseSuppression: true,
+    sampleRate: {ideal: 48000, min: 35000},
   }
 
   try {
